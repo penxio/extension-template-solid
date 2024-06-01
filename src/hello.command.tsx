db670d3754fe12fb771aef469de6d605bddfbc97
0xzion <0xzion.penx@gmail.com>
2024-06-01 23:53:45 +0800

    initial commit

---
+++ src/hello.command.tsx
@@ -0,0 +1,3 @@
+export function Main() {
+  return <div class="text-green-500 bg-amber-100 text-4xl">Hello Solid!</div>
+}
---
+++ src/index.command.tsx
@@ -0,0 +1,3 @@
+export function Main() {
+  return <div class="text-blue-500 bg-amber-100">Hello World!</div>
+}
